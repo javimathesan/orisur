0e1b59ddf3b8f50403c3fb827e413181275155f1
Javimathesan <javiermatheuscursos@gmail.com>
2026-09-06 20:03:33 -0300

    se integra Navbar en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Orisur</h1>
       <p>Gadgets y tecnología portátil importada de Asia para Argentina.</p>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
