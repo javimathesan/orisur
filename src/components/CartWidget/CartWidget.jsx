f8c6f85b0292011b5c360178cefaf7f8d9f8829b
Javimathesan <javiermatheuscursos@gmail.com>
2026-09-06 18:45:19 -0300

    feat: se crea el componente CartWidget y se elimina .gitkeep de components

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+// Componente que muestra el ícono del carrito y la cantidad de ítems
+function CartWidget() {
+  return (
+    <span className="cart-widget">
+      🛒 <b>3</b>
+    </span>
+  );
+}
+
+export default CartWidget;
