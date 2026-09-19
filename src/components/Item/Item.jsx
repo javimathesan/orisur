8e751d78a7d4d08c4a91b821003d363233143c2e
Javimathesan <javiermatheuscursos@gmail.com>
2026-09-19 02:23:22 -0300

    feat: envolver contenido de Item en Link hacia /item/:id para navegar al detalle

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,19 +1,21 @@
-import "./Item.css"; // Busca el CSS correcto 
+import { Link } from "react-router-dom";
+import "./Item.css";
 
 const Item = ({ product }) => {
-  const { name, price, img, category, description, stock } = product;
+  const { id, name, price, img, category, description, stock } = product;
 
   return (
     <div className="item">
-      <img className="item__img" src={img} alt={name} />
-      <h3 className="item__name">{name}</h3>
-      <p className="item__category">{category}</p>
-      <p className="item__description">{description}</p>
-      <p className="item__price">${price}</p>
-      <p className="item__stock">Stock: {stock}</p>
+      <Link to={`/item/${id}`} className="item__link">
+        <img className="item__img" src={img} alt={name} />
+        <h3 className="item__name">{name}</h3>
+        <p className="item__category">{category}</p>
+        <p className="item__description">{description}</p>
+        <p className="item__price">${price}</p>
+        <p className="item__stock">Stock: {stock}</p>
+      </Link>
     </div>
   );
 };
 
 export default Item;
-
