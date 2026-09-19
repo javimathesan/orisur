86a6bbe2d34d2c323ea7a75dde4c62ecdf21dd65
Javimathesan <javiermatheuscursos@gmail.com>
2026-09-19 01:35:19 -0300

    feat: configurar rutas principales con react-router-dom (Home, Category, Item, NotFound)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-// src/App.jsx
-import Navbar from "./components/Navbar/Navbar";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <ItemListContainer greeting="¡Bienvenido a orisur! Encontrá la mejor tecnología portátil importada." />
-      <Footer />
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ItemListContainer greeting="¡Bienvenido a Orisur!" />} />
+          <Route path="category/:categoryId" element={<ItemListContainer greeting="¡Bienvenido a Orisur!" />} />
+          <Route path="item/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h2>404 - Página no encontrada</h2>
+      <p>El producto o la categoría que buscás no existe.</p>
+    </div>
+  )
+}
+
+export default NotFound
