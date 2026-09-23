0cf391bfbf58f6b630f44c2d00a5e637a75f6a3d
Javimathesan <javiermatheuscursos@gmail.com>
2026-09-23 00:04:42 -0300

    feat: crear componente Checkout con formulario controlado y ruta /checkout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import Cart from './components/Cart/Cart'
+import Checkout from './components/Checkout/Checkout'
 import NotFound from './components/NotFound/NotFound'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path="category/:categoryId" element={<ItemListContainer greeting="¡Bienvenido a Orisur!" />} />
             <Route path="item/:id" element={<ItemDetailContainer />} />
             <Route path="cart" element={<Cart />} />
+            <Route path="checkout" element={<Checkout />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
