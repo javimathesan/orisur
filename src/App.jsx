981304664b8816d41d2bf3f049936da095b89011
Javimathesan <javiermatheuscursos@gmail.com>
2026-09-22 21:53:14 -0300

    feat: vamos a envolver la app con CartProvider para acceso global al carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartProvider } from './context/CartContext'
 import Layout from './components/Layout/Layout'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
@@ -6,18 +7,19 @@ import NotFound from './components/NotFound/NotFound'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<ItemListContainer greeting="¡Bienvenido a Orisur!" />} />
-          <Route path="category/:categoryId" element={<ItemListContainer greeting="¡Bienvenido a Orisur!" />} />
-          <Route path="item/:id" element={<ItemDetailContainer />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<ItemListContainer greeting="¡Bienvenido a Orisur!" />} />
+            <Route path="category/:categoryId" element={<ItemListContainer greeting="¡Bienvenido a Orisur!" />} />
+            <Route path="item/:id" element={<ItemDetailContainer />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   )
 }
 
 export default App
-
